d6897049a7d419f567a7e8209db02d82e861066a
Humberto <hlsp999@gmail.com>
2021-10-01 08:56:33 +0100

    Fix argument to be only the unique code for public land mobile network

--- lib/home_network_identity.rb
+++ lib/home_network_identity.rb
@@ -4,17 +4,16 @@ require 'data/mcc_mnc'
 class HomeNetworkIdentity
   include MccMnc
 
-  def initialize(mcc:, mnc: nil)
-    @mcc = mcc.to_s.rjust(3, "0")
-    @mnc = mnc.to_s.rjust(2, "0")
+  def initialize(plmn:)
+    @plmn = plmn.to_s
   end
 
   def country
-    easy_find("mcc")&.fetch("countryName", nil)
+    raw_by_plmn.first&.fetch("countryName", nil)
   end
 
   def iso_country_code
-    easy_find("mcc")&.fetch("countryCode", nil)
+    raw_by_plmn.first&.fetch("countryCode", nil)
   end
 
   def operator
@@ -22,15 +21,10 @@ class HomeNetworkIdentity
   end
 
   def raw_by_plmn
-    find_per_column("plmn", @mcc + @mnc)
+    find_per_column("plmn", @plmn)
   end
 
   private
-
-  def easy_find(column)
-    value = instance_variable_get("@#{column}")
-    find_per_column(column, value).first
-  end
 
   def find_per_column(column, value)
     mcc_mnc_data.select do |row|
--- test/test_home_network_identity.rb
+++ test/test_home_network_identity.rb
@@ -3,28 +3,26 @@ require 'home_network_identity'
 
 class HomeNetworkIdentityTest < Minitest::Test
   def test_country
-    mcc = 603
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc)
+    plmn = 60301
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_equal "Algeria", home_network_identity.country
   end
 
   def test_iso_country_code
-    mcc = 268
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc)
-    assert_equal "PT", home_network_identity.iso_country_code
+    plmn = 23403
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
+    assert_equal "GG", home_network_identity.iso_country_code
   end
 
   def test_operator
-    mcc = 268
-    mnc = 1
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc, mnc: mnc)
+    plmn = 26801
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_equal "Vodafone Portugal", home_network_identity.operator
   end
 
   def test_raw_by_plmn
-    mcc = 268
-    mnc = 1
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc, mnc: mnc)
+    plmn = 26801
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_equal(
       [{"plmn"=>"26801", "nibbledPlmn"=>"62F810", "mcc"=>"268", "mnc"=>"01", "region"=>"Europe", "type"=>"National", "countryName"=>"Portugal", "countryCode"=>"PT", "lat"=>"39.5", "long"=>"-8", "brand"=>"Vodafone", "operator"=>"Vodafone Portugal", "status"=>"Operational", "bands"=>"GSM 900 / UMTS 900 / UMTS 2100 / LTE 800 / LTE 1800 / LTE 2600", "notes"=>"formerly Telecel (2001)"}],
       home_network_identity.raw_by_plmn
@@ -32,29 +30,26 @@ class HomeNetworkIdentityTest < Minitest::Test
   end
 
   def test_operator_with_strings
-    mcc = "268"
-    mnc = "1"
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc, mnc: mnc)
+    plmn = "26801"
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_equal "Vodafone Portugal", home_network_identity.operator
   end
 
   def test_mcc_not_found
-    mcc = 268234
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc)
+    plmn = 268234
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_nil home_network_identity.iso_country_code
   end
 
   def test_operator_with_mcc_mnc_pair_not_found
-    mcc = 268
-    mnc = 111
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc, mnc: mnc)
+    plmn = 268111
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_nil home_network_identity.operator
   end
 
   def test_raw_by_plmn_with_mcc_mnc_pair_not_found
-    mcc = 268
-    mnc = 111
-    home_network_identity = HomeNetworkIdentity.new(mcc: mcc, mnc: mnc)
+    plmn = 268111
+    home_network_identity = HomeNetworkIdentity.new(plmn: plmn)
     assert_equal [], home_network_identity.raw_by_plmn
   end
 
